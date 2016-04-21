f53bdf1e4f2abc9c1000137d6dd2269cdeedb245
Hyperlazydude <AnimeAzn101@gmail.com>
2016-04-20 22:28:31 -0700

    PinSettingActivity Class for trying to enter the setting and created the layout

--- app/src/main/java/com/autism/mindpower/feeling/PinActivity.java
+++ app/src/main/java/com/autism/mindpower/feeling/PinActivity.java
@@ -8,9 +8,11 @@ import android.support.v4.content.SharedPreferencesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by David Nguyen on 4/20/2016.
+ * Background work to storage the pin number when first register
  */
 public class PinActivity extends AppCompatActivity {
     EditText password;
@@ -23,10 +25,11 @@ public class PinActivity extends AppCompatActivity {
     }
 
     public void register(View view) {
-        SharedPreferences sharedpreference = getSharedPreferences("pin#", Context.MODE_PRIVATE);
+        SharedPreferences sharedpreference = getSharedPreferences("pinNumber", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedpreference.edit();
         editor.putString("password", password.getText().toString());
 
         editor.commit();
+        Toast.makeText(this, "Created Pin", Toast.LENGTH_SHORT).show();
     }
 }
