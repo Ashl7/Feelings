a3fee1667261c6adc6058b56b9e8d0eb45c44ad5
ASHL7 <ashl_777@yahoo.com>
2017-01-05 14:24:03 -0800

    updated Contact.java

--- app/src/main/java/com/autism/mindpower/feeling/Contact.java
+++ app/src/main/java/com/autism/mindpower/feeling/Contact.java
@@ -6,28 +6,19 @@ package com.autism.mindpower.feeling;
  */
 public class Contact {
 
-    private String number = null;
-    private String name = null;
+    private String number;
+    private String name;
 
     public Contact(String number, String name) {
         this.number = number;
         this.name = name;;
     }
 
-    //getters and setters
     public String getNumber() {
         return number;
     }
 
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
